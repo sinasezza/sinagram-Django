8024c8c6f799649528395ff89e7e4a1122e9b4ba
sinasezza <s.e.sezza121@gmail.com>
2024-03-23 17:11:16 +0330

    chore(deps): poetry added for package managing and requirements updated.

--- sinagram/urls.py
+++ sinagram/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('messenger.urls',namespace='messenger')),
+    path('', include('messenger.urls', namespace='messenger')),
     
 ]
 
