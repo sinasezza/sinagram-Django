723e502c41eee116f5fbfbcf56bafcc619de60da
sinasezza <s.e.sezza121@gmail.com>
2024-03-23 18:02:03 +0330

    ref(templates): templates refactored.

--- sinagram/urls.py
+++ sinagram/urls.py
@@ -16,14 +16,17 @@ Including another URLconf
 from django.contrib import admin 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path,include
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('messenger.urls', namespace='messenger')),
-    
+    path('', views.index_view, name='index'),
+    path('users/', include('users.urls', namespace='users')),
+    path('messenger/', include('messenger.urls', namespace='messenger')),
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
